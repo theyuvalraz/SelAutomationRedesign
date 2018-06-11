dea35bcdaeb87c9b5663d2929a5313f44d0be3cb
Yuval Raz <yuvalzhe@gmail.com>
2018-06-11 19:07:09 +0300

    Page Interface added

---
+++ SelAutomationRedesign/Framework/UI/Ipage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+
+namespace SelAutomationRedesign.Framework.UI
+{
+    interface IPage
+    {
+        IWebDriver Driver { get; }
+        Page Navigate();
+        bool IsTextPresent(string text);
+    }
+}
---
+++ SelAutomationRedesign/Framework/UI/Page.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+
+namespace SelAutomationRedesign.Framework.UI
+{
+    class Page
+    {
+        public IWebDriver Driver { get; }
+
+        public Page( IWebDriver driverValue )
+        {
+            Driver = driverValue;
+        }
+
+        public virtual Page Navigate()
+        {
+            return this;
+        }
+
+        public bool IsTextPresent( string text )
+        {
+
+            return true;
+        }
+    }
+}
