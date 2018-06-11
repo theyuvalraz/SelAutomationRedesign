40318e8c5a19a828f7d41e0f9e7b8d22debd473b
Yuval Raz <yuvalzhe@gmail.com>
2018-06-11 18:17:09 +0300

    Add project files.

---
+++ SelAutomationRedesign/SelAutomation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SelAutomationRedesign
+{
+    [TestFixture]
+    public class SelAutomation
+    {
+        private IWebDriver _driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            _driver = new ChromeDriver();
+        }
+
+
+        [TearDown]
+        public void TearDown()
+        {
+            _driver.Quit();
+        }
+
+
+        [TestCase()]
+        public void TestOne()
+        {
+            _driver.Navigate().GoToUrl("http://www.google.com");
+        }
+
+    }
+}
