cfb8990eaf3b1fc0ac8d83adaa1c3e46717c4f18
Yuval Raz <yuvalzhe@gmail.com>
2018-07-21 03:04:19 +0300

    Update SelAutomation.cs

--- SelAutomationRedesign/SelAutomation.cs
+++ SelAutomationRedesign/SelAutomation.cs
@@ -15,7 +15,7 @@ namespace SelAutomationRedesign
             var options = new ChromeOptions();
             options.AddArguments("--lang=en-GB", "start-maximized");
             ATDriver = AutomationDriverFactory.Get();
-            ATDriver.WebDriver = new ChromeDriver("tools\\chromedrive",options);
+            ATDriver.WebDriver = new ChromeDriver("$JENKINS_HOME\\tools\\chromedriver",options);
             ;
         }
 
